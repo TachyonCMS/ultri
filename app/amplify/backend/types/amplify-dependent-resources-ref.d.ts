fae0d176c62c7b054c6b99adf4f845c3d0a6abec
Brian Winkers <bwinkers@gmail.com>
2022-09-30 14:33:18 -0700

    Amplify Dependent Resources Attributes

--- app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,18 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "userPoolGroups": {
+            "adminGroupRole": "string",
+            "techGroupRole": "string"
+        },
+        "ultriauthorization": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string",
+            "CreatedSNSRole": "string"
+        }
+    }
+}
