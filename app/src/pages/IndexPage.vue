f9a3d7ca414f06a2d3c6472b323de12b30792574
Brian Winkers <bwinkers@gmail.com>
2022-10-02 18:15:29 -0700

    Basic homepage for sign up

--- app/src/pages/IndexPage.vue
+++ app/src/pages/IndexPage.vue
@@ -1,11 +1,13 @@
 <template>
   <q-page class="row items-center justify-evenly">
-    <!-- <ColorSwatches></ColorSwatches> -->
+<q-card flat class="bg-card-surround justify-center text-center">
+ <q-card-section header ><h1 class="q-ma-sm q-ma-sm-md q-ma-md-lg q-ma-lg-xl">Express yourself</h1></q-card-section>
+ <q-card-section ><h2 class="q-ma-sm q-ma-sm-md  q-ma-md-lg q-ma-lg-xl">Start sharing today.</h2></q-card-section>
+ <q-card-actions class="justify-center" ><q-btn size="lg" class="bg-cta on-cta q-ma-sm q-ma-sm-md q-ma-md-lg q-ma-lg-xl" to="/auth/signup">Create your site</q-btn> </q-card-actions>
+</q-card>
   </q-page>
 </template>
 
 <script setup>
-import ColorSwatches from "../components/ColorSwatches.vue";
-
 import { ref } from "vue";
 </script>
