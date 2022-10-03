7142a5b105c5694362670aaf7b63626b10bf2c98
Brian Winkers <bwinkers@gmail.com>
2022-10-02 18:16:39 -0700

    Main name is Ultri

--- app/src/stores/layout.js
+++ app/src/stores/layout.js
@@ -2,7 +2,7 @@ import { defineStore } from "pinia";
 
 export const useLayoutStore = defineStore("layout", {
   state: () => ({
-    title: "Tachyon CMS"
+    title: "Ultri"
   }),
   getters: {},
   actions: {
